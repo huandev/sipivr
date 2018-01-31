ceb89e68a90420f1590ea0595e670aeaf48f02f4
okarpukhin <o.karpukhin@yandex.ru>
2018-01-31 10:27:06 +0300

    Script module uses sub folders

--- sipivr.plugins/sipivr.script/src/main/java/ru/sipivr/script/rest/ScriptApiController.java
+++ sipivr.plugins/sipivr.script/src/main/java/ru/sipivr/script/rest/ScriptApiController.java
@@ -1,5 +1,6 @@
 package ru.sipivr.script.rest;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,6 +9,7 @@ import ru.sipivr.core.controller.BaseController;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.Collection;
 
 @PreAuthorize("isAuthenticated()")
 @RequestMapping("/api")
@@ -16,7 +18,7 @@ public class ScriptApiController extends BaseController {
     @RequestMapping(value = "/scripts", produces = "application/json")
     @ResponseBody
     public String[] scripts() {
-        File[] files = new File(appConfig.getScriptPath()).listFiles((dir, name) -> name.endsWith(".js"));
-        return Arrays.stream(files).map(f -> new File(appConfig.getScriptPath()).toURI().relativize(new File(f.getPath()).toURI()).getPath()).toArray(String[]::new);
+        Collection<File> files = FileUtils.listFiles(new File(appConfig.getScriptPath()), new String[]{"js"}, true);
+        return files.stream().map(f -> new File(appConfig.getScriptPath()).toURI().relativize(new File(f.getPath()).toURI()).getPath()).toArray(String[]::new);
     }
 }
